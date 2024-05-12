26eb6672d75f6b72b30be1bd5b645456019e25a4
Dzulfiqar Fathur Rahman <social@dzulfiqarfr.com>
2024-05-12 15:36:06 +0700

    Remove namespace call for base functions

--- src/helper/group_tracks.R
+++ src/helper/group_tracks.R
@@ -1,9 +1,9 @@
 # Put tracks into groups of 100
 group_tracks <- function(data) {
-  base::stopifnot(tibble::is_tibble(data))
+  stopifnot(tibble::is_tibble(data))
   
-  if (!base::any("track_id" == base::names(data))) {
-    base::stop("`data` must contain `track_id` column.", call. = FALSE)
+  if (!any("track_id" == names(data))) {
+    stop("`data` must contain `track_id` column.", call. = FALSE)
   }
   
   data |>
@@ -11,15 +11,11 @@ group_tracks <- function(data) {
     dplyr::mutate( # Use row number to group tracks
       row_number = dplyr::row_number(),
       group = dplyr::case_when(
-        row_number %in% base::seq(
-          1,
-          base::length(base::unique(track_id)),
-          99
-        ) ~ row_number
+        row_number %in% seq(1, length(unique(track_id)), 99) ~ row_number
       )
     ) |> 
     tidyr::fill(group) |> 
-    base::split(~ group) |>
+    split(~ group) |>
     purrr::map(\(x) dplyr::pull(x, track_id)) |>
     purrr::map(\(x) stringr::str_flatten(x, collapse = ","))
 }
